d122d73f572cdbb6109b57df1b73f4311239a0b9
Sudheer <165461530+Sudheer0746@users.noreply.github.com>
2025-03-20 21:02:34 +0530

    Time: 1 ms (99.88%), Space: 53.4 MB (21.25%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-         int candidate = nums[0];
-         int count = 1;
-         for( int i=1; i<nums.length; i++)
-         {
-            if( nums[i] == candidate)
-            {
-                count++;
-            }else
-            {
-                count--;
+        int candidate = nums[0];  // Start with the first element as the candidate
+        int count = 1;  // Count starts at 1 since we already have one occurrence of the candidate
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == candidate) {
+                count++;  // Increase count if the element is the same as the candidate
+            } else {
+                count--;  // Decrease count if the element is different
+                if (count == 0) {
+                    candidate = nums[i];  // Reset candidate if count drops to 0
+                    count = 1;  // Reset count to 1 for the new candidate
+                }
             }
-            if( count == 0)
-            {
-                candidate = nums[i];
-                count =1;
-            }
-         }
-         return candidate;
+        }
+        
+        return candidate;  // The majority element will be the candidate
     }
 }
