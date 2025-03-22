921d0d1616ae182dceeae9892a83aa3deadefd27
Sudheer <165461530+Sudheer0746@users.noreply.github.com>
2025-03-22 22:54:07 +0530

    Time: 1 ms (99.84%), Space: 61.6 MB (63.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,12 +4,12 @@ class Solution {
         int maxProfit = 0;
         for( int i=0; i<prices.length; i++)
         {
-            // int sellPrice = Math.max( )
+ // lets first check whether the current prices of stock is greater than previously bought stock or not if greater then it is beneficial to sell the stock there is always some profit makes 
             if( buyPrice < prices[i])
             {
                 int profit = prices[i] - buyPrice;
                 maxProfit = Math.max( maxProfit, profit);
-            }else {
+            }else { //if current price is lower than the previously bought stocks then need to more buy stocks 
                 buyPrice = prices[i];
             }
         }
