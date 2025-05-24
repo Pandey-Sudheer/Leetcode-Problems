0d833529e352e5b35022039a48f67bed163280f6
Sudheer <165461530+Sudheer0746@users.noreply.github.com>
2025-05-24 20:35:37 +0545

    Time: 1 ms (72.54%), Space: 45.6 MB (63.69%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,10 +2,12 @@ class Solution {
     public void setZeroes(int[][] matrix) {
       int n = matrix.length;
       int m = matrix[0].length;
-      
+
+    //   make the every  row and column false intially
       boolean row[] = new boolean[n];
       boolean col[] = new boolean[m];
 
+    //   check whether item of row or column have '0' item or not if yes then set the item true
       for( int i=0; i<n; i++)
       {
         for( int j=0; j<m; j++)
@@ -17,7 +19,7 @@ class Solution {
             }
         }
       }
-
+    //   making the '0' of its entire row thoose have true value
       for( int i=0; i<n; i++)
       {
         if( row[i])
@@ -28,7 +30,7 @@ class Solution {
             }
         }
       }
-
+//   making the '0' of its entire column thoose have true value
       for( int j=0; j<m; j++)
       {
         if( col[j])
